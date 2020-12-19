7de2d96793036ef237c77bf2f7c9b0f471812c42
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-19 21:49:42 +0100

    add signup_path to user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :users
+
+  #users routes
+  get     '/signup',  to: 'users#new'
+  post    '/signup',  to: 'users#create'
+  resources :users, only: [ :show, :new, :create]  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

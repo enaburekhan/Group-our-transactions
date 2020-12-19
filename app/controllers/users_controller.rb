dbc25fbab015495541e1f84a04eb1a2a2cef5495
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-19 21:41:30 +0100

    add logic to user create action and modify index.html.erb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: %i[index edit update destroy]
 
   # GET /users
   # GET /users.json
-  def index
-    @users = User.all
-  end
+  # def index
+  #   @users = User.all
+  # end
 
   # GET /users/1
   # GET /users/1.json
@@ -18,48 +19,45 @@ class UsersController < ApplicationController
   end
 
   # GET /users/1/edit
-  def edit
-  end
+  # def edit
+  # end
 
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        log_in @user
+        redirect_to shopping_lists_path, notice: 'User was successfully created.' 
       else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        redirect_to signup_path, notice: @user.errors[:username].first
       end
-    end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def update
+  #   respond_to do |format|
+  #     if @user.update(user_params)
+  #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
+  #       format.json { render :show, status: :ok, location: @user }
+  #     else
+  #       format.html { render :edit }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # DELETE /users/1
   # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @user.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ApplicationRecord
+  has_many :shopping_lists, foreign_key: :author_id, dependent: :destroy
+  has_many :groups, dependent: :destroy
+  
+  validates :username, presence: { message: 'Username must be given' },
+                       length: { in: 4..20, message: 'Username length must be between 4 to 20 characters' },
+                       uniqueness: { message: 'Username already exists' },
+                       format: { with: /\A[a-zA-Z0-9]+\z/, message: 'Username must be alphanumeric' }
+  
+  def all_total_amount
+    shopping_lists.where(active: true).sum(:price)
+  end
+  
+  def list_shopping_lists_join_groups
+    new_list = []
+    shopping_lists.includes(:shopping_lists_groups).each do |item|
+      new_list << item unless Shopping_listsGroup.find_by(shopping_list_id: item.id).present?
+    end
+    new_list
+  end
+  
+  def shopping_lists_order_recent
+    shopping_lists.includes(:shopping_lists_groups).order(created_at: :desc).where(active: true)
+  end
+  
+  def shopping_lists_order_ancient
+    shopping_lists.includes(:shopping_lists_groups).order(:created_at).where(active: true)
+  end    
 end

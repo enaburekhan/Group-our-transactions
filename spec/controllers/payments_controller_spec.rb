c56be823830a7fe2d39d0bc328b6594e6b2e380e
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-21 20:41:41 +0100

    create payments_controller_spec

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
-RSpec.describe "Payments", type: :request do
-
-end
+RSpec.describe PaymentsController, type: :controller do
+    describe ' controller test cases ' do
+      fixtures :shopping_lists
+      fixtures :users
+      fixtures :groups
+  
+      let(:user1) { users(:one) }
+      let(:user2) { users(:two) }   
+      let(:user) { FactoryBot.create :user }
+      let(:group1) { groups(:one) }
+      let(:shopping_list1) { shopping_lists(:one) }
+  
+      before { log_in user }
+      before { log_in user }
+      before { log_in user2 }
+  
+      describe '#create' do
+        it 'redirects to shopping_lists_url' do
+          new_payment = FactoryBot.build :payment
+          post :create, params: {
+            payment: {
+              transaction_code: new_payment.transaction_code,
+              amount: new_payment.amount,
+              shopping_list_id: shopping_list1.id
+            }
+          }
+          expect(response).to redirect_to shopping_lists_path
+        end
+      end
+    end
+  end

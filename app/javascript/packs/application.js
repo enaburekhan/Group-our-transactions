01cccdcb9411b9b04e53eefa68c67284c0ac85da
enaburekhan <ericenaburekhan2018@gmail.com>
2020-12-23 17:37:28 +0100

    edit application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,9 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "@fortawesome/fontawesome-free/js/all";
 import "bootstrap";
-import "../stylesheets/application";
+import "../stylesheets/application.css.scss";
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
